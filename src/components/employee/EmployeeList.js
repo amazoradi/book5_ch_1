f582e8ed3f2331af388c152e84275b9258075523
amazoradi <amazoradi@gmail.com>
2018-11-16 11:34:40 -0600

    first react practice complete

---
+++ src/components/employee/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <article>
+        <h1>Employee List</h1>
+        <section>Jessica Younker</section>
+        <section>Jordan Nelson</section>
+        <section>Zoe LeBlanc</section>
+        <section>Blaise Roberts</section>
+      </article>
+    );
+  }
+}
+
+
+export default EmployeeList
---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class LocationList extends Component {
+  render() {
+    return(
+      <section>
+        <h4> Nashville Location</h4>
+        <ul>
+          <li>Nashville North, 123 Fictitious Ave N, Nashville TN</li>
+          <li>Nashville South, 999 Imagiation St S, Nashville TN</li>
+        </ul>
+      </section>
+    )
+  }
+}
+
+export default LocationList
